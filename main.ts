696645eb19f99da1077f7d0c1b551b37fa1cf00e
gorpeln <339332@163.com>
2023-12-02 19:17:21 +0800

    no message

--- main.ts
+++ main.ts
@@ -39,6 +39,7 @@ if (await exists(fullPath)) {
 // 保存原始数据
 const queswordsAll = mergeWords(words, wordsAlreadyDownload);
 await Deno.writeTextFile(fullPath, JSON.stringify(queswordsAll));
+console.log(queswordsAll); // 输出数据
 
 // 更新 README.md
 const readme = await createReadme(queswordsAll);
